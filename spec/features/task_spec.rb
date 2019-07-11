b90e567f6b0bfa564b98d98b3519465a3166fcfe
Danny Cheng-Hsuan Han <30727089+dannyh79@users.noreply.github.com>
2019-07-11 23:15:32 +0800

    步驟9: 將網站中的中文部分共用化 (#5)
    * Step 9: i18n-izing the app
    
    * Took out the excessive "#{}" around i18n strings in views
    
    * Removed the uneccessary "#{}"s in feature spec
    
    * Removed the unwanted "#{}" in app/helpers/tasks_helper.rb
    
    * Fixed the broken test from resolving conflicts

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -19,29 +19,29 @@ RSpec.describe Task, type: :feature do
       visit tasks_path
       expect(page).to have_css(
                                 'table tbody tr:first-child td:first-child', 
-                                text: "#{Task.first.title}"
+                                text: Task.first.title
                               )
       expect(page).to have_css(
                                 'table tbody tr:first-child td:nth-child(5)', 
-                                text: "#{Task.first.description}"
+                                text: Task.first.description
                               )
       
       expect(page).to have_css(
                                 'table tbody tr:nth-child(2) td:first-child', 
-                                text: "#{Task.second.title}"
+                                text: Task.second.title
                               )
       expect(page).to have_css(
                                 'table tbody tr:nth-child(2) td:nth-child(5)', 
-                                text: "#{Task.second.description}"
+                                text: Task.second.description
                               )
       
       expect(page).to have_css(
                                 'table tbody tr:nth-child(3) td:first-child', 
-                                text: "#{Task.third.title}"
+                                text: Task.third.title
                               )
       expect(page).to have_css(
                                 'table tbody tr:nth-child(3) td:nth-child(5)', 
-                                text: "#{Task.third.description}"
+                                text: Task.third.description
                               )
     end
   end
@@ -52,25 +52,37 @@ RSpec.describe Task, type: :feature do
 
       expect(Task.first.title).to eq title
       expect(Task.first.description).to eq description
-      expect(page).to have_content('Success: a new task is created!')
+      expect(page).to have_content(I18n.t("tasks.create.notice"))
       expect(page).to have_content(title)
       expect(page).to have_content(description)
     end
 
     it 'without input' do
       expect{ create_task_with(nil, nil) }.not_to change{ Task.count }
-      expect(page).to have_content('Title can\'t be blank')
-      expect(page).to have_content('Description can\'t be blank')
+      expect(page).to have_content("
+        #{I18n.t("activerecord.attributes.task.title")} 
+        #{I18n.t("activerecord.errors.models.task.attributes.title.blank")}
+      ")
+      expect(page).to have_content("
+        #{I18n.t("activerecord.attributes.task.description")} 
+        #{I18n.t("activerecord.errors.models.task.attributes.description.blank")}
+      ")
     end
     
     it 'without title' do
       expect{ create_task_with(nil, description) }.not_to change{ Task.count }
-      expect(page).to have_content('Title can\'t be blank')
+      expect(page).to have_content("
+        #{I18n.t("activerecord.attributes.task.title")} 
+        #{I18n.t("activerecord.errors.models.task.attributes.title.blank")}
+      ")
     end
     
     it 'without description' do
       expect{ create_task_with(title, nil) }.not_to change{ Task.count }
-      expect(page).to have_content('Description can\'t be blank')
+      expect(page).to have_content("
+        #{I18n.t("activerecord.attributes.task.description")} 
+        #{I18n.t("activerecord.errors.models.task.attributes.description.blank")}
+      ")
     end
   end
 
@@ -94,34 +106,46 @@ RSpec.describe Task, type: :feature do
       expect(Task.first.title).to eq new_title
       expect(Task.first.description).to eq new_description
       
-      expect(page).to have_content('Success: the task is updated!')
+      expect(page).to have_content(I18n.t("tasks.update.notice"))
       expect(page).to have_content(new_title)
       expect(page).to have_content(new_description)
     end
 
     it 'without input' do
       expect { edit_task_with(nil, nil) }.not_to change { Task.first }
-      expect(page).to have_content('Title can\'t be blank')
-      expect(page).to have_content('Description can\'t be blank')
+      expect(page).to have_content("
+        #{I18n.t("activerecord.attributes.task.title")} 
+        #{I18n.t("activerecord.errors.models.task.attributes.title.blank")}
+      ")
+      expect(page).to have_content("
+        #{I18n.t("activerecord.attributes.task.description")} 
+        #{I18n.t("activerecord.errors.models.task.attributes.description.blank")}
+      ")
     end
     
     it 'without title' do
       expect { edit_task_with(nil, new_description) }.not_to change { Task.first }
-      expect(page).to have_content('Title can\'t be blank')
+      expect(page).to have_content("
+        #{I18n.t("activerecord.attributes.task.title")} 
+        #{I18n.t("activerecord.errors.models.task.attributes.title.blank")}
+      ")
     end
     
     it 'without description' do
       expect { edit_task_with(new_title, nil) }.not_to change { Task.first }
-      expect(page).to have_content('Description can\'t be blank')
+      expect(page).to have_content("
+        #{I18n.t("activerecord.attributes.task.description")} 
+        #{I18n.t("activerecord.errors.models.task.attributes.description.blank")}
+      ")
     end
   end
 
   describe 'delete a task' do
     it do
       create_task_with(title, description)
-      expect{ click_on 'Delete' }.to change{ Task.count }.by(-1)
+      expect{ click_on I18n.t("tasks.table.delete") }.to change{ Task.count }.by(-1)
 
-      expect(page).to have_content('Success: the task is deleted!')
+      expect(page).to have_content(I18n.t("tasks.destroy.notice"))
       # check if there's still remnant info, from the deleted, in views
       expect(page).not_to have_content(title)
       expect(page).not_to have_content(description)
@@ -133,17 +157,17 @@ RSpec.describe Task, type: :feature do
   def create_task_with(title, description)
     visit new_task_path
     within('form.form_task') do
-      fill_in 'Title', with: title
-      fill_in 'Description', with: description
-      click_on 'Create Task'
+      fill_in I18n.t("tasks.table.title"), with: title
+      fill_in I18n.t("tasks.table.description"), with: description
+      click_on I18n.t("helpers.submit.task.create", model: I18n.t("activerecord.models.task"))
     end
   end
 
   def edit_task_with(new_title, new_description)
     within('form.form_task') do
-      fill_in 'Title', with: new_title
-      fill_in 'Description', with: new_description
-      click_on 'Update Task'
+      fill_in I18n.t("tasks.table.title"), with: new_title
+      fill_in I18n.t("tasks.table.description"), with: new_description
+      click_on I18n.t("helpers.submit.task.update", model: I18n.t("activerecord.models.task"))
     end
   end
 
